a8c644a1799f75f337c505239c235716fad85496
labo2002 <labo2002@usherbrooke.ca>
2022-04-03 12:23:54 -0400

    Adding procedures and view

--- TS_imm.sql
+++ TS_imm.sql
@@ -1,8 +1,52 @@
 -- Obtenir la recette R d'une cuvée C
+CREATE OR REPLACE FUNCTION ObtenirRecette
+  (idC Cuvee_id)
+  RETURNS norecette
+LANGUAGE SQL AS
+$$
+    SELECT noserier
+    FROM cuvee NATURAL JOIN recette
+    WHERE idcuvee = idC
+$$;
 
--- Obtenir tous les ingrédients I d'une recette R
+-- Obtenir tous les ingrédients pour chaque recette
+CREATE OR REPLACE VIEW IngrRecette AS
+    SELECT recette.noserier AS NoRecette, ingredient.nom AS Nom
+FROM recette JOIN etape ON recette.noserier = etape.idrecette
+             JOIN s_etape USING (idetape)
+             JOIN action USING (id_setape)
+             JOIN alimentactionre USING (idaction)
+             JOIN alimentre USING (idaliment)
+             JOIN ingredient ON alimentre.idaliment = ingredient.idingredient;
+
+-- Obtenir tous les ingrédients pour une recette donnée
+CREATE OR REPLACE FUNCTION ObtenirIngredients
+  (idR norecette)
+  RETURNS nom_type
+LANGUAGE SQL AS
+$$
+    SELECT Nom
+FROM IngrRecette
+    WHERE NoRecette = idR;
+$$;
 
 -- Ajouter une nouvelle recette
+CREATE OR REPLACE PROCEDURE CreerRecette
+    (idR norecette, aut nom_type)
+    LANGUAGE SQL AS
+$$
+    INSERT INTO recette (noserier, auteur) VALUES
+    (idR, aut)
+$$;
+
+-- Ajouter une nouvelle étape
+CREATE OR REPLACE PROCEDURE CreerEtape
+    (idE etape_id, descr text, idR norecette)
+    LANGUAGE SQL AS
+$$
+    INSERT INTO etape (idetape, description, idrecette) VALUES
+    (idE, descr, idR)
+$$;
 
 -- Obtenir le fabriquant F d'un équipement
 
--- TS_inv.sql
+++ TS_inv.sql
@@ -1,1 +1,35 @@
--- Fichier de creation de donnees
+INSERT INTO recette (noserier, auteur) VALUES
+('ABCD', 'Olivier');
+
+INSERT INTO etape (idetape, description, idrecette) VALUES
+('QWER', 'ma recette', 'ABCD');
+
+INSERT INTO s_etape (id_setape, description, idetape) VALUES
+('TYUI', 'une sous-etape', 'QWER');
+
+INSERT INTO action (idaction, duree, debut, description, id_setape) VALUES
+('OPAS', current_date::timestamp, current_date::timestamp, 'une action', 'TYUI');
+
+INSERT INTO ingredient (idingredient, nom) VALUES
+('DFGH', 'pomme');
+
+INSERT INTO alimentactionre (idaliment, idaction) VALUES
+('DFGH', 'OPAS');
+
+INSERT INTO alimentre (idaliment, quantite) VALUES
+('DFGH', 13);
+
+
+
+CREATE OR REPLACE VIEW RecetteEtape AS
+    SELECT *
+FROM recette JOIN etape ON recette.noserier = etape.idrecette;
+
+CREATE OR REPLACE VIEW EtapeSousetape AS
+    SELECT idetape, etape.description AS DescriptionEtape, id_setape, s_etape.description AS DescriptionSousEtape
+FROM etape JOIN s_etape USING (idetape);
+
+CREATE OR REPLACE VIEW SousetapeAction AS
+    SELECT idAction, action.description AS DescriptionAction, duree, debut,
+           s_etape.id_setape, s_etape.description AS DescriptionSousEtape
+FROM action JOIN s_etape USING (id_setape);
